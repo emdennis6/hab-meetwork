af1c0aa6742c6088da88b3c36ef4b963c9aab800
Elicia Dennis <lici.dennis@gmail.com>
2016-02-06 16:18:49 -0700

    things and stuff

--- Meetwork/app/src/main/java/hi/meetwork/MainActivity.java
+++ Meetwork/app/src/main/java/hi/meetwork/MainActivity.java
@@ -9,11 +9,14 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.firebase.client.Firebase;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Firebase.setAndroidContext(this);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -26,6 +29,9 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        Firebase ref = new Firebase("https://fiery-heat-5087.firebaseio.com/");
+        ref.child("users").child("alanisawesome");
     }
 
     @Override

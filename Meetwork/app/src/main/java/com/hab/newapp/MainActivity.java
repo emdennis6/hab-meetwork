76ede5f918f371062aae4a00e00e760af9bf069a
Peter <pac96@users.noreply.github.com>
2016-02-07 09:12:42 -0500

    Merged files with stuff

--- Meetwork/app/src/main/java/com/hab/newapp/MainActivity.java
+++ Meetwork/app/src/main/java/com/hab/newapp/MainActivity.java
@@ -1,22 +1,20 @@
 package hi.meetwork;
 
-import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.os.Bundle;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button loginBtn;
+    private Button signupBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
         loginBtn = (Button) findViewById(R.id.button);
         signupBtn = (Button) findViewById(R.id.button2);
@@ -30,7 +28,7 @@ public class MainActivity extends AppCompatActivity {
      * Transitions into the Existing login page
      */
     private void button1Click() {
-        startActivity(new Intent("hi.meetwork.ExistingLoginActivity"));
+        startActivity(new Intent("com.hab.newapp.ExistingLoginActivity"));
     }
 
     /**
@@ -38,7 +36,7 @@ public class MainActivity extends AppCompatActivity {
      * Transitions into the Sign up page
      */
     private void button2Click() {
-        startActivity(new Intent("hi.meetwork.SignUpActivity"));
+        startActivity(new Intent("com.hab.newapp.SignUpActivity"));
     }
 
     /**
@@ -55,4 +53,6 @@ public class MainActivity extends AppCompatActivity {
                 break;
         }
     }
+
+
 }
